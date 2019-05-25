f4b896adedad5e7fd5c6b3fa034e1597e88f1e19
Krzysztof Suszyński <krzysztof.suszynski@wavesoftware.pl>
2019-05-25 02:13:20 +0200

    Initial code import
    Preparation to publish to Maven Central.

---
+++ log4j2/src/main/java/pl/wavesoftware/log4j2/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * @author <a href="mailto:krzysztof.suszynski@wavesoftware.pl">Krzysztof Suszynski</a>
+ * @since 0.1.0
+ */
+
+@ParametersAreNonnullByDefault
+@ReturnTypesAreNonnullByDefault
+package pl.wavesoftware.log4j2;
+
+import pl.wavesoftware.eid.api.ReturnTypesAreNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
